3d28277a589783512cd82ed36e3409354a3b6b3d
Ismam Hasan Ovi <121273617+oviii-001@users.noreply.github.com>
2024-06-17 01:45:14 +0600

    Delete anonymous-messages directory

--- anonymous-messages/scripts.js
+++
@@ -1,9 +0,0 @@
-
-
-document.getElementById('messageForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const message = document.getElementById('messageInput').value;
-    document.getElementById('messageDisplay').innerText = `Anonymous Message: ${message}`;
-    document.getElementById('messageInput').value = '';
-});
-
